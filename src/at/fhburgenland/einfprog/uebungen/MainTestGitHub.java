1d07950fbc3292f34accf96c5d20705f2aafc017
Daniel260601 <47327755+Daniel260601@users.noreply.github.com>
2021-11-11 12:59:50 +0100

    New TestCommit for a Test File :D

---
+++ src/at/fhburgenland/einfprog/uebungen/MainTestGitHub.java
@@ -0,0 +1,7 @@
+package at.fhburgenland.einfprog.uebungen;
+
+public class MainTestGitHub {
+    public static void main(String[] args) {
+        System.out.println("This is a Test for GitHub");
+    }
+}
